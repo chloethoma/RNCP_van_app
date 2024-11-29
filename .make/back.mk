6490388f45b70855f2c9746c7f804ae9520977c3
chloe.thomas <chloe.thomas@believe.com>
2024-11-29 15:48:53 +0100

    ops(make): setup make commands

---
+++ .make/back.mk
@@ -0,0 +1,36 @@
+## —— Backend Command ————————————————————————————————————————————————————————————————
+
+# —— Back Docker
+php-sh: ## Connect to the FrankenPHP container
+	@$(PHP_CONT) sh
+
+php-bash: ## Connect to the FrankenPHP container via bash so up and down arrows go to previous commands
+	@$(PHP_CONT) bash
+
+test: ## Start tests with phpunit, pass the parameter "c=" to add options to phpunit, example: make test c="--group e2e --stop-on-failure"
+	@$(eval c ?=)
+	@$(DOCKER_COMP) exec -e APP_ENV=test php bin/phpunit $(c)
+
+
+# —— Composer
+composer: ## Run composer, pass the parameter "c=" to run a given command, example: make composer c='req symfony/orm-pack'
+	@$(eval c ?=)
+	@$(COMPOSER) $(c)
+
+vendor: ## Install vendors according to the current composer.lock file
+vendor: c=install --prefer-dist --no-dev --no-progress --no-scripts --no-interaction
+vendor: composer
+
+
+# —— Symfony
+sf: ## List all Symfony commands or pass the parameter "c=" to run a given command, example: make sf c=about
+	@$(eval c ?=)
+	@$(SYMFONY) $(c)
+
+cc: c=c:c ## Clear the cache
+cc: sf
+
+# —— Postgres
+psql: ## Commande "psql" : ouvre une session terminal pour accéder à la base de données PostgreSQL
+	@echo "Connexion à la base de données PostgreSQL..."
+	@$(DB_CONT) psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
---
+++ .make/front.mk
@@ -0,0 +1,8 @@
+## —— Frontend Command ————————————————————————————————————————————————————————————————
+
+# —— Front Docker
+front-sh: ## Connect to the FrankenPHP container
+	@$(FRONT_CONT) sh
+
+front-bash: ## Connect to the FrankenPHP container via bash so up and down arrows go to previous commands
+	@$(FRONT_CONT) bash
