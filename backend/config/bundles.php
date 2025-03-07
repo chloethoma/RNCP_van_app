e3dad920e82e9ab9b0e373c31351f73a3da05a3e
chloe.thomas <chloe.thomas@believe.com>
2025-03-07 14:58:04 +0100

    chore(back): install profiler pack

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
