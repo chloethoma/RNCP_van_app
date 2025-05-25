7d5a9e45054bd683af35e4c24691c7370897bdfe
chloe.thomas <chloe.thomas@believe.com>
2025-05-25 16:39:59 +0200

    feat(VS-203-F): legal views

--- frontend/src/components/bottomNavBar/BottomNavBar.tsx
+++ frontend/src/components/bottomNavBar/BottomNavBar.tsx
@@ -1,8 +1,10 @@
-import { Home, Users, User, Settings } from "lucide-react";
+import { Home, Users, User } from "lucide-react";
 import NavItem from "./NavItem";
+import Footer from "../Footer";
 
 function BottomNavBar() {
   return (
+    <>
     <nav className="fixed bottom-0 left-0 w-full bg-white flex rounded-t justify-around pt-3 pb-9 z-20 border-t-1 border-border-grey">
       <NavItem to={"/"} label={"Home"}>
         <Home size={24} />
@@ -15,11 +17,11 @@ function BottomNavBar() {
       <NavItem to={"/profile"} label={"Mon Profil"}>
         <User size={24} />
       </NavItem>
-
-      <NavItem to={"/settings"} label={"Paramètres"}>
-        <Settings size={24} />
-      </NavItem>
     </nav>
+    
+    <Footer/>
+    </>
+
   );
 }
 
